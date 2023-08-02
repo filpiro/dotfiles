7b3a4fb3fd91d330fe2334318af69e26f04cc9f3
filippo.purelab <filippo.pirola@purelab.it>
2023-08-02 11:44:13 +0200

    adding start

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -1,12 +1,17 @@
 # Docker
 alias dcup="docker compose up -d"
-alias dcstop="docker compose stop"
+# alias dcstop="docker compose stop"
 
-function dev-restart-fn(){
+function dc-stop-fn(){
+  docker stop $(docker ps --format '{{.Names}}')
+}
+alias dcstop="dc-stop-fn"
+
+function dc-restart-fn(){
   docker compose down
   docker compose up -d --build
 }
-alias dcrestart="dev-restart-fn"
+alias dcrestart="dc-restart-fn"
 
 function dex-fn {
   docker exec -it -u $UID $(docker ps --format '{{.Names}}' | grep ${1:-php}) bash
