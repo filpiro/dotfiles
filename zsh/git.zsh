5a1146eb314e3bbac79c477e75cb4873e9733b76
filippo.purelab <filippo.pirola@purelab.it>
2024-09-26 11:08:51 +0200

    refactor git

---
+++ zsh/git.zsh
@@ -0,0 +1,10 @@
+# Git
+alias gpat="git push --all && git push origin --tags"
+
+# Git Flow
+alias gfw="git flow"
+alias gfwfs="git flow feature start"
+alias gfwfp="git flow feature publish"
+alias gfwff="git flow feature finish"
+alias gfwrs="git flow release start"
+alias gfwrf="git flow release finish"
